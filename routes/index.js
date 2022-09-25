45e8ff7a3474f65a4dfb1a2d06e62199d68c4e23
ccastro18 <ccastro892@soyudemedellin.edu.co>
2022-09-25 15:02:26 -0500

    install mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const myappController = require('../controllers/myappController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Titulo' });
-});
+router.get('/', myappController.index);
+
 
 module.exports = router;
